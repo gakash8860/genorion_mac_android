33487053ed76ef978b4b46bbee32cc7c921a509b
Akash <gakash8860@gmail.com>
2022-12-04 16:01:54 +0530

    Websocket branch changes

--- lib/TempUser/showtemp.dart
+++ lib/TempUser/showtemp.dart
@@ -168,6 +168,7 @@ class _ShowTempUserState extends State<ShowTempUser> {
                           temp[index].name.toString(),
                         ),
                         subtitle: Text(temp[index].email.toString()),
+                        trailing: Text("${temp[index].date?.toString().substring(0,10)}${"\n"}${temp[index].timing.toString()}"),
                         leading: IconButton(
                           icon: const Icon(
                             Icons.delete_forever,
@@ -210,13 +211,6 @@ class _ShowTempUserState extends State<ShowTempUser> {
                 deleteDid = temp[index].dId.toString();
                 deleteMobile = temp[index].mobile.toString();
 
-                if (kDebugMode) {
-                  print("deletePid $deletePid");
-                  print("deleteFid $deleteFid");
-                  print("deleteFltid $deleteFltid");
-                  print("deleteRid $deleteRid");
-                  print("deleteDid $deleteDid");
-                }
                 await deleteTempUser();
                 Navigator.pop(context);
               }),
