9a1cb469398d1f171b0af13d607c848986f0e66c
Akash <gakash8860@gmail.com>
2022-12-18 13:07:29 +0530

    Master

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:genorion_mac_android/StartingScreen/frontscreen.dart';
 import 'package:genorion_mac_android/cooldrop.dart';
 
 
@@ -121,7 +122,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: changeDone?changeColor:Colors.blue,
       ),
-      home: const CoolDrop(),
+      home: const GettingStartedScreen(),
     );
   }
 }
